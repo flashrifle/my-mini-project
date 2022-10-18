3a669a8df1253f460d99c5f3f7d3a40d0b94fe3c
flashrifle <flashrifle@gmail.com>
2022-10-18 20:40:33 +0900

    '로그아웃_기능구현'

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -10,6 +10,12 @@ def login_view(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             print("인증 성공")
+            login(request, user)
         else:
             print("인증 실패")
     return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("user:login")

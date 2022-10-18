96c12e3b47c95c8fed0de80ef542bc00d44f31a6
flashrifle <flashrifle@gmail.com>
2022-10-18 21:39:50 +0900

    '회원가입_기능구현'

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from .models import User
+
 
 # Create your views here.
 
@@ -19,3 +21,20 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("user:login")
+
+def signup_view(request):
+    if request.method =="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        email = request.POST["email"]
+
+        user = User.objects.create_user(username, email, password)
+        user.last_name = lastname
+        user.first_name = firstname
+        user.save()
+
+        return redirect("user:login")
+
+    return render(request, "users/signup.html")
